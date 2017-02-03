419a707a5ad9ed02912da3bf717aa1728ea48e9d
Krishna Biradar <cod3ncoff33@gmail.com>
2017-02-03 12:56:57 +0530

    made a separate directory for all the images

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 function digitalclock() {
     var timenow = new Date();
     var seconds = timenow.getSeconds().toString();
-    document.getElementById("clock").innerHTML = timenow.toLocaleTimeString();
+    document.getElementById("digital-clock").innerHTML = timenow.toLocaleTimeString();
 }
 window.onload = function() {
     setInterval(digitalclock, 1000);
     var timenow = new Date();
     var seconds = timenow.getSeconds().toString();
-    
+
     // add necessary css and attributes to animate the radar-clock hand..
     $("#rad").css("animation-delay", "-" + seconds + "s");
     $(".center").append("<style> @keyframes radar-clock {  from { transform: rotate(0deg);} to {transform: rotate(360deg);} </style>");
