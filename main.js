52ba1a5e93ee9834ad88c82bd911729115a7e225
Krishna Biradar <cod3ncoff33@gmail.com>
2017-02-03 12:49:26 +0530

    Removed unnecessary code and prettified all files.

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ window.onload = function() {
     setInterval(digitalclock, 1000);
     var timenow = new Date();
     var seconds = timenow.getSeconds().toString();
+    
+    // add necessary css and attributes to animate the radar-clock hand..
     $("#rad").css("animation-delay", "-" + seconds + "s");
     $(".center").append("<style> @keyframes radar-clock {  from { transform: rotate(0deg);} to {transform: rotate(360deg);} </style>");
 };
